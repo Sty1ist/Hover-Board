5828dd3b4747d849e4023036071050e4ff6f8db2
Ins <imbalancerm@gmail.com>
2021-09-23 00:15:38 +0300

    реализовал рандомный подбор заданых цветов

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const board = document.querySelector('#board')
+const colors = ['red', 'blue', 'green', 'yellow', 'purple']
 const SQUARES_NUMBER = 500
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
-    board.append(square)
+
 
     square.addEventListener('mouseover', () =>
         setColor(square))
+
+    square.addEventListener('mouseleave', () =>
+        removeColor(square))
 
     board.append(square)
 }
 
 function setColor(element) {
-    element.style.backgroundColor = 'red'
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
 }
